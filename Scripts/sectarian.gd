280663d9802195e115ea2b0677ed1d026bb28ef6
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-09-22 08:00:41 -0600

    Godot 4.3, tilemaplayer and begin of enemies movements

--- Scripts/sectarian.gd
+++ Scripts/sectarian.gd
@@ -1,12 +1,33 @@
 extends CharacterBody2D
 
+@onready var ray_horizontal = $RayHorizontal
+@onready var ray_vertical = $RayVertical
 
-const SPEED = 300.0
+
+enum Movement {
+	NONE,
+	HORIZONTAL,
+	VERTICAL
+}
+
+@export var movement_type := Movement.NONE
+@export var SPEED = 900.0
+
+var direction = Vector2.ZERO
 
 
 func _ready():
 	add_to_group("enemies")
 
+	if movement_type == Movement.HORIZONTAL:
+		direction = Vector2.RIGHT
+	elif movement_type == Movement.VERTICAL:
+		direction = Vector2.UP
+
 func _physics_process(delta):
-	velocity = Vector2.ZERO
+	if ray_horizontal.is_colliding() or ray_vertical.is_colliding():
+		direction = -direction
+
+	velocity = direction * SPEED * delta
+
 	move_and_slide()
