8918bb83f58a09ec1a6f521646c91d5e72e0eba4
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-11-23 08:12:15 -0600

    lava
    lava fix
    
    fak

--- Scripts/lava.gd
+++ Scripts/lava.gd
@@ -1,29 +1,28 @@
 extends Node2D
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var timer: Timer = $Timer
-@onready var point_light_2d: PointLight2D = $PointLight2D
-
-@export var damage = 500
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var mirror_cooldown: Timer = $MirorCooldown
+@onready var damage_cooldown: Timer = $DamageCooldown
+@onready var point_light: PointLight2D = $PointLight2D
 
 var is_flipped_v := false
-
-func _ready() -> void:
-	timer.start()
-
 var time_passed: float = 0.0
 
-func _on_timer_timeout() -> void:
-	sprite_2d.flip_v = is_flipped_v
-	is_flipped_v = !is_flipped_v
-	timer.start()
+func _ready() -> void:
+	mirror_cooldown.start()
 
 func _process(delta: float) -> void:
 	time_passed += delta
-	point_light_2d.energy = abs(sin(time_passed) - 0.2) + 0.2
+	point_light.energy = abs(sin(time_passed)) * 0.15
 
+
+func _on_timer_timeout() -> void:
+	sprite.flip_v = is_flipped_v
+	is_flipped_v = !is_flipped_v
+	mirror_cooldown.start()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area is Hitbox and area.owner.is_in_group(&"player"):
+	if area is Hitbox and area.owner.is_in_group(&"player") and damage_cooldown.is_stopped():
 		var attack = Attack.new(1, Vector2.ZERO)
-		var damage_given = area.damage(attack)
+		area.damage(attack)
+		damage_cooldown.start()
