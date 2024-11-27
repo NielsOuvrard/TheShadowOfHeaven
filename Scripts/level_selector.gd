39040ba8e0e873da39930547f6e9b348bd2c4049
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-11-26 23:46:55 -0600

    level selector mouse selection

--- Scripts/level_selector.gd
+++ Scripts/level_selector.gd
@@ -8,6 +8,7 @@ extends CanvasLayer
 
 const SCREEN_X_SIZE = 240
 const NUMBER_NIVELS = 3
+const CANVAS_SCALE = 8
 var image_size_x : int
 
 var locked_portraits := []
@@ -22,6 +23,9 @@ func change_selected_level(value):
 	if timer.is_stopped():
 		timer.start()
 		selected = selected + value
+		change_it()
+
+func change_it():
 		portrait.frame = selected
 		level_text.frame = selected
 
@@ -51,6 +55,14 @@ func _ready() -> void:
 	level_text.position.x = (SCREEN_X_SIZE / 2) - image_size_x
 	portrait.frame = 0
 	level_text.frame = 0
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var size = portrait.texture.get_size().x * CANVAS_SCALE
+		var part = int(event.position.x / size * NUMBER_NIVELS)
+		if part != selected:
+			selected = part
+			change_it()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed('move_right'):
